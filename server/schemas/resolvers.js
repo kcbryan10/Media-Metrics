fbdd284054465f5abcdbf14b7f5085c822a4a109
justineicher <justineicher@gmail.com>
2022-02-26 12:23:58 -0600

    finished resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,6 +15,25 @@ const resolvers = {
         return userData;
       }
     },
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+        .populate('thoughts')
+        .populate('friends');
+    },
+    user: async (parent, {username}) => {
+      return User.findOne({username})
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
+    thoughts: async (parent, {username}) => {
+      const params = username ? {username} : {};
+      return Thought.find(params).sort({createdAt: -1});
+    },
+    thought: async (parent, {_id}) => {
+      return Thought.findOne({_id});
+    },
   },
 
   Mutation: {
