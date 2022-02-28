89b1a27ed825fb9f43faf6daddef1b7affc33a2b
kcbry <kcbryan10@gmail.com>
2022-02-27 19:07:45 -0600

    added jsx to function aswell as formated coding

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,8 +8,29 @@ import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
 
 
 const Home = () => {
-  const { loading, data } = useQuery(QUERY_THOUGHTS);
-  const { data: userData } = useQuery(QUERY_ME_BASIC);
-  const thoughts = data?.thoughts || [];
+    const { loading, data } = useQuery(QUERY_THOUGHTS);
+    const { data: userData } = useQuery(QUERY_ME_BASIC);
+    const thoughts = data?.thoughts || [];
 
-  const loggedIn = Auth.loggedIn();
+    const loggedIn = Auth.loggedIn();
+
+
+    return (
+        <main>
+            <div className='homepage-content'>
+                <div>
+                    <h4 className="homepage-titles">About Us</h4>
+                    <div className='homepage-desc'>
+                        <p className='home-text'>description</p>
+                    </div>
+                </div>
+                <div>
+                    <h4 className="homepage-titles"> How to Reach Us</h4>
+                    <div className='homepage-desc'>
+                        <p className='home-text'>description</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+};
