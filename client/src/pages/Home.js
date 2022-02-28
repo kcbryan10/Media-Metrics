ac4e0224115eb132ed00df24380ff752b438cefb
kcbry <kcbryan10@gmail.com>
2022-02-27 19:06:51 -0600

    added Home function with consts for data loading

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,4 +7,9 @@ import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
 
 
+const Home = () => {
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+  const { data: userData } = useQuery(QUERY_ME_BASIC);
+  const thoughts = data?.thoughts || [];
 
+  const loggedIn = Auth.loggedIn();
