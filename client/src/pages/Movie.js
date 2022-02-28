48ba0f7ffdc92495e6b6c7bdb2dcf55705be03ba
kcbry <kcbryan10@gmail.com>
2022-02-27 19:38:30 -0600

    added template to game/movie pages

---
+++ client/src/pages/Movie.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ThoughtList from '../components/ThoughtList';
+import ThoughtForm from '../components/ThoughtForm';
+
+import Auth from '../utils/auth';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
+
+const Movie = () => {
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+  const { data: userData } = useQuery(QUERY_ME_BASIC);
+  const thoughts = data?.thoughts || [];
+
+  const loggedIn = Auth.loggedIn();
+
+  return (
+    <main>
+    <div>
+      {loggedIn && (
+        <div>
+          <ThoughtForm />
+        </div>
+      )}
+      <div>
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <ThoughtList
+            thoughts={thoughts}
+            title="Thought List"
+          />
+        )}
+      </div>
+    </div>
+  </main>
+  );
+};
+
+export default Movie;
