1b8fd8224d5e34d44c8d1668d0dc28639d1da556
kcbry <kcbryan10@gmail.com>
2022-03-01 14:13:41 -0600

    updated css to be easier to track

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,57 +3,35 @@ import React from 'react';
 const Home = () => {
 
     return (
-        <main className='homepage'>
-                <section id="what-we-do" className="intro">
-      <div className="flex-row">
-        <h2 className="section-title primary-border">
-          What We Do
-        </h2>
-      </div>
-      <div className="flex-row">
-        <p>
-          butcher selfies chambray shabby chic gentrify readymade yr Echo Park XOXO Tumblr normcore Banksy direct trade  Blue Bottle chillwave you probably haven't heard of them single-origin coffee Vice fanny pack fixie Odd Future Austin fingerstache pickled twee synth Wes Anderson Thundercats viral bitters flannel meggings narwhal Marfa Schlitz sustainable Intelligentsia umami deep v craft
-        </p>
-      </div>
-    </section>
-
-    <section id="what-you-do" className="steps">
+  <main className='homepage'>
+    <section id="what-you-do" className="processs">
       <div className="flex-row">
         <h2 className="section-title secondary-border">
           Start Blogging!
         </h2> 
       </div>
-      <div className="step">
-        <h3>Step 1.</h3>
-        <div className="step-info">
-          <div className="step-img">
-            <img src="./assets/images/step-1.svg" alt="" />
-          </div>
-          <div className="step-text">
+      <div className="process">
+        <h3>step 1</h3>
+        <div className="process-info">
+          <div className="process-text">
             <h4>Sign up</h4>
             <p>sign up on our state of the art sign up page!</p>
           </div>
         </div>
       </div>
-      <div className="step">
-        <h3>Step 2.</h3>
-        <div className="step-info">
-          <div className="step-img">
-            <img src="./assets/images/step-2.svg" alt="" />
-          </div>
-          <div className="step-text">
+      <div className="process">
+        <h3>step 2</h3>
+        <div className="process-info">
+          <div className="process-text">
             <h4>Start Blogging</h4>
             <p>Go to one of are three blog pages!</p>
           </div>
         </div>
       </div>
-      <div className="step">
-        <h3>Step 4.</h3>
-        <div className="step-info">
-          <div className="step-img">
-            <img src="./assets/images/step-4.svg" alt="" />
-          </div>
-          <div className="step-text">
+      <div className="process">
+        <h3>step 3</h3>
+        <div className="process-info">
+          <div className="process-text">
             <h4>React to Posts</h4>
             <p>react to posts by either commenting or giving it a thumbs up or down!</p>
           </div>
@@ -61,46 +39,42 @@ const Home = () => {
       </div>
     </section>
 
-     <section id="your-trainers">
+     <section id="your-developers">
       <div className="flex-row">
         <h2 className="section-title primary-border">
           Developers
         </h2>
       </div>
 
-      <div className="trainers">
-        <article className="trainer">
-          <img src="./assets/images/trainer-1.jpg" alt="Bryan" />
-          <div className="trainer-bio">
-            <h3 className="trainer-name">Bryan Keller</h3>
-            <h4 className="trainer-role">FrontEnd</h4>
+      <div className="developers">
+        <article className="developer">
+          <div className="developer-bio">
+            <h3 className="developer-name">Bryan Keller</h3>
+            <h4 className="developer-role">FrontEnd</h4>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi neque animi quo cupiditate commodi saepe culpa sed itaque velit maiores optio dolorem excepturi aperiam dolores, voluptatibus suscipit amet quis repellat!</p>
           </div>
         </article>
 
-         <article className="trainer">
-          <img src="./assets/images/trainer-1.jpg" alt="Justin" />
-          <div className="trainer-bio">
-            <h3 className="trainer-name">Justin Eicher</h3>
-            <h4 className="trainer-role">BackEnd</h4>
+         <article className="developer">
+          <div className="developer-bio">
+            <h3 className="developer-name">Justin Eicher</h3>
+            <h4 className="developer-role">BackEnd</h4>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi neque animi quo cupiditate commodi saepe culpa sed itaque velit maiores optio dolorem excepturi aperiam dolores, voluptatibus suscipit amet quis repellat!</p>
           </div>
         </article>
 
-        <article className="trainer">
-          <img src="./assets/images/trainer-2.jpg" alt="Jeramy" />
-          <div className="trainer-bio">
-            <h3 className="trainer-name">Jeramy</h3>
-            <h4 className="trainer-role">BackEnd</h4>
+        <article className="developer">
+          <div className="developer-bio">
+            <h3 className="developer-name">Jeramy</h3>
+            <h4 className="developer-role">BackEnd</h4>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi neque animi quo cupiditate commodi saepe culpa sed itaque velit maiores optio dolorem excepturi aperiam dolores, voluptatibus suscipit amet quis repellat!</p>
             </div>
           </article>
 
-        <article className="trainer">
-          <img src="./assets/images/trainer-3.jpg" alt="Djamel" />
-          <div className="trainer-bio">
-            <h3 className="trainer-name">Djamel Ramdani</h3>
-            <h4 className="trainer-role">FrontEnd</h4>
+        <article className="developer">
+          <div className="developer-bio">
+            <h3 className="developer-name">Djamel Ramdani</h3>
+            <h4 className="developer-role">FrontEnd</h4>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi neque animi quo cupiditate commodi saepe culpa sed itaque velit maiores optio dolorem excepturi aperiam dolores, voluptatibus suscipit amet quis repellat!</p>
           </div>
         </article>
