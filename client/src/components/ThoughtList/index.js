a24ee2e37fe2dbc3d2a31dc3d200c9f4a10ed06f
kcbry <kcbryan10@gmail.com>
2022-03-01 13:01:01 -0600

    added better homepage, styled thought form on music page

--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const ThoughtList = () => {  
+const ThoughtList = ({ thoughts, title }) => {
+  if (!thoughts.length) {
+    return <h3 className='no-thoughts'>No Blogs Yet</h3>;
+  }
+
   return (
-    <main>
-      <div/>
-    </main>
+    <div>
+      <h3>{title}</h3>
+      {thoughts &&
+        thoughts.map(thought => (
+          <div key={thought._id} className="">
+            <p className="">
+              <Link
+                to={`/profile/${thought.username}`}
+                style={{ fontWeight: 700 }}
+                className=""
+              >
+                {thought.username}
+              </Link>{' '}
+              created on {thought.createdAt}
+            </p>
+            <div className="card-body">
+              <Link to={`/thought/${thought._id}`}>
+                <p>{thought.thoughtText}</p>
+                <p className="mb-0">
+                  Reactions: {thought.reactionCount} || Click to{' '}
+                  {thought.reactionCount ? 'see' : 'start'} the discussion!
+                </p>
+              </Link>
+            </div>
+          </div>
+        ))}
+    </div>
   );
 };
 
