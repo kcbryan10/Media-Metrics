6d0df7f88c493db15334c28c242ac53f9d9be4b5
Jeramy Sena <jeramy_sena@hotmail.com>
2022-03-02 20:33:54 -0600

    updated server file

--- server/server.js
+++ server/server.js
@@ -12,14 +12,6 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV) {
-  app.use(express.static(path.join(__dirname, '../client')));
-}
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client'));
-});
-
 
 const startServer = async () => {
   const server = new ApolloServer({
@@ -31,6 +23,7 @@ const startServer = async () => {
   server.applyMiddleware({ app });
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 
+
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
@@ -38,4 +31,13 @@ const startServer = async () => {
   });
 };
 
+  // Serve up static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+  
 startServer()
